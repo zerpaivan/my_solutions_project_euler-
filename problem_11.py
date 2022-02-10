0d7af38778498bb0b9887e14df1a15760a47475c
zerpaian <zerpaivan@gmail.com>
2022-02-10 18:07:53 -0400

    created function largest_product problem 11

--- problem_11.py
+++ problem_11.py
@@ -11,7 +11,26 @@ def load_grid(grid_path):
     file_grid.close()
     return grid
 
+
+def largest_product(n_elements, grid):
+    len_rows = len(grid)
+    len_columns = len(grid[0])
+    print(len_rows, len_columns)
+    for n in range(len_rows):
+        for m in range(len_columns):
+            H = []
+            V = []
+            if m + n_elements <= len_columns:
+                for e in range(n_elements):
+                    H.append(grid[n][m + e])
+                print("H", H)
+            if n + n_elements <= len_rows:
+                for e in range(n_elements):
+                    V.append(grid[n + e][m])
+                print("V", V)
+
+
+
+
 grid_n = load_grid(grid_path)
-print(grid_n)
-# grip = file_grip.readlines()
-# print(grip)
+largest_product(4, grid_n)
