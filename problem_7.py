398c0b630f934f47eadef54c81c20abaa3674540
zerpaian <zerpaivan@gmail.com>
2022-02-09 20:53:53 -0400

    update problem 7

--- problem_7.py
+++ problem_7.py
@@ -1,15 +1,17 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
 # that the 6th prime is 13.
 # What is the 10 001st prime number?
+from math import sqrt, ceil
+import time
+t_star = time.time()
 
 
 def isPrime(num):
     is_prime = True
-    if num == 2:
-        return is_prime
-
-    for factor in range(2, 1 + num // 2):
-        if num % factor == 0:
+    for factor in range(2, 1 + ceil(num // sqrt(num))):
+        if num == 2:
+            return isPrime
+        elif num % factor == 0:
             is_prime = False
             break
     return is_prime
@@ -29,3 +31,6 @@ def x_prime_num(place):
 
 if __name__ == "__main__":
     print(x_prime_num(10001))
+
+t_end = time.time()
+print("time: ", t_end - t_star)
